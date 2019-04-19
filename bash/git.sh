543821bdd5b202ae4713ebec85c27584641ab611
David Qu <davidqu@blend.com>
2019-04-19 13:59:24 -0700

    Add git aliases

---
+++ bash/git.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+alias gb="git branch --sort=-committerdate --color=always | head -n 8"
+alias gs="git status"
+alias gch="git checkout"
+alias gcm="git commit"
+alias gp="git pull"
+alias ga="git add"
