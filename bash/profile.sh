049fd38c196ddd16e6c9c72815ebf48694c9d1f7
David Qu <dqu@goguardian.com>
2023-02-01 13:32:19 -0800

    alias go-coverage

--- bash/profile.sh
+++ bash/profile.sh
@@ -2,7 +2,8 @@ export CLICOLOR=1
 export LSCOLORS=ExGxBxDxCxEgEdxbxgxcxd
 # alias random-email='time >> ~/emails.log && echo "dqu+$(uuidgen | tr A-Z a-z | cut -c1-8)@email.com"| tee >(pbcopy) >> ~/emails.log'
 alias last-email="tail ~/emails.log"
-alias copy-last-cmd="fc -ln -1 | xsel --clipboard"
+alias copy-last-cmd="fc -ln -1 | pbcopy"
+alias go-coverage="go tool cover -func=go-coverage.out | tail -n1 | awk '{print $3}' | grep -Eo '[0-9.]+'"
 
-source git.sh
-source docker.sh
+source ~/github/dqu123/profile/bash/git.sh
+source ~/github/dqu123/profile/bash/docker.sh
